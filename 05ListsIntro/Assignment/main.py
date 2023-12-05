b4e7f3f42a4d83a24d9a6fc5d092dae66f865d01
Logan Loomis <27loganloomis@usd266.com>
2023-12-05 09:07:13 -0600

    completed some assignments

--- 05ListsIntro/Assignment/main.py
+++ 05ListsIntro/Assignment/main.py
@@ -1,10 +1,12 @@
+
+print("make_abc")
 def make_abc():
     result = ["a","b","c"]
     return result
 
 print(make_abc())
 
-print("################")
+print("equal_edges")
 
 def equal_edges(num1, num2, num3, num4):
 
@@ -19,7 +21,7 @@ print(equal_edges(6,9,4,6))
 print(equal_edges(8,2,3,1))
 
 
-print("################")
+print("common_edge")
 
 
 def common_edge(list1, list2):
@@ -28,12 +30,120 @@ def common_edge(list1, list2):
     else: 
         return False
     
-print(common_edge())
+print(common_edge([1,2,3],[3,2,1]))
+print(common_edge([1,2,3],[4,2,7]))
+print(common_edge([8,7,2],[2,4,5]))
+
+
+print("all_the_same")
     
 
 
+def all_the_same(list1):
+    if list1[0] == list1[1] and list1[0] == list1[2]:
+        return True
+    else: 
+        return False
+    
+print(all_the_same([1,1,1]))
+print(all_the_same([6,3,6]))
+print(all_the_same([9,9,9]))
+
+
+
+print("all_unique")
+
+
+
+def all_unique(list1):
+    if list1[0] != list1[1] and list1[0] != list1[2]:
+        return True
+    else: 
+        return False
+    
+print(all_unique([1,2,3]))
+print(all_unique([6,3,6]))
+print(all_unique([9,9,9]))
+print(all_unique([8,2,3]))
+
+
+print("Increasing")
 
 
 
 
+def increasing(list1):
+    if list1[2] > list1[1] and list1[2] > list1[0]:
+        return True
+    else:
+        return False
+print(increasing([1,2,3]))
+print(increasing([5,5,5]))
+print(increasing([-1,0,1]))
 
+
+
+print("all_true")
+
+def all_true(list1):
+    if list1[0] == "True" and list1[1] == "True" and list1[2] == "True":
+        return True
+    else:
+        return False
+
+print(all_true(["True","True","True"]))
+print(all_true(["False","True","True"]))
+print(all_true(["False","False","True"]))
+
+
+
+
+print("mostly_true")
+
+def mostly_true(list1):
+
+    first = list1[0]
+    middle = list1[1]
+    last = list1[2]
+    
+
+    if first == "True" and middle == "True" or  first == "True" and last == "True" or middle == "True" and first == "True" or middle == "True":
+        return True
+    else:
+        return False
+print(mostly_true(["True","False","True"]))
+print(mostly_true(["False","False","False"]))
+print(mostly_true(["True","True","True"]))
+print(mostly_true(["False","True","False"]))
+print(mostly_true(["True","False","False"]))
+
+print("make_copy")
+
+def make_copy(list1):
+    return list1[0], list1[1], list1[2]
+
+print(make_copy([1,2,3]))
+
+print("repeat_thrice")
+def repeat_thrice(list1):
+    return list1[0], list1[0], list1[0]
+print(repeat_thrice([1]))
+
+print("make_reversed_copy")
+
+def make_reversed_copy(list1):
+    return list1[2], list1[1], list1[0]
+print(make_reversed_copy([1,2,3]))
+
+print("reverse_in_place")
+
+def reverse_in_place(list1):
+    first = list1[2]
+    last = list1[0]
+    middle = list1[1]
+    newList = [first, middle, last]
+
+
+    return(newList)
+
+print(reverse_in_place([1,2,3]))
---
+++ 06ListsAndLoops/Assignment/main.py
@@ -0,0 +1,123 @@
+def count_failing_grades(percents):
+    count = 0
+    for percents in percents:
+        if percents < 60:
+            count = count + 1
+    return count
+
+inputList = ([90,67,40,23,59,89])
+returnvalue = count_failing_grades(inputList)
+print(returnvalue)
+
+print("################")
+def count_act_scores(scores):
+    count = 0
+    for score in scores:
+        if score <= 36 and score >= 1:
+            count = count + 1
+    return count
+
+print(count_act_scores([45,12,34,-6]))
+
+print("###############")
+def number_sum(numbers):
+    total = 0
+    for number in numbers:
+        total = total + number
+    return total
+print(number_sum([5,3,2,5]))
+
+print("##############")
+def average_act_score(scores):
+        count = 0
+        totalScore = 0
+        for score in scores:
+            if score <= 36 and score >= 1:
+                count = count + 1
+                totalScore = totalScore + score
+                avg = totalScore / count
+        return avg
+print(average_act_score([1,45,23,32,90]))
+print("################")
+def all_true(list1):
+    for TorF in list1:
+        if TorF == "False":
+            return False
+    else:
+        return True
+print(all_true(["True","True","True"]))
+print(all_true(["False","False","True"]))
+print(all_true(["True","True","True"]))
+print("############")
+def any_true(list1):
+        for TorF in list1:
+            if TorF == "True":
+                return True
+        else:
+         return False
+        
+print(any_true(["False","True","True"]))
+print(any_true(["False","False","True"]))
+print(any_true(["False","False","False"]))
+print("###########")
+def mostly_true(list1):
+    TCount = 0
+    FCount = 0
+    for TorF in list1:
+        if TorF == "True":
+            TCount = TCount + 1
+        elif TorF == "False":
+            FCount = FCount + 1
+    if TCount > FCount:
+        return True
+    else:
+        return False
+    
+print(mostly_true(["True","False","False","False"]))
+print(mostly_true(["True","True","True","False"]))
+print(mostly_true(["True","True","False","False"]))
+      
+print("###########")
+
+def has_vowel(List1):
+    for vowel in List1:
+        if vowel in ["a","e","i","o","u"]:
+            return True
+        else:
+            return False
+        
+print(has_vowel(["a","t","w"]))
+print(has_vowel(["i","g","p"]))
+print(has_vowel(["q","r","w"]))
+print("#############")
+def all_the_same(list1):
+    first = list1[0]
+    for number in list1:
+        if first != list1[1]:
+            return False
+        
+        return True
+            
+        
+print(all_the_same([1,1,1]))
+print(all_the_same([1,2,3]))
+print(all_the_same([5,6,3]))
+
+print("############")
+def increasing(list1):
+    first = list1[0]
+    previous = list[0]-1
+    for num in list1:
+        if previous > first:
+            return True
+        return False
+    
+print(increasing([1,2,2]))
+print(increasing([3,2,1]))
+print(increasing([1,2,3,4,5]))
+  
+
+#def is_incrementing():
+
+
+
