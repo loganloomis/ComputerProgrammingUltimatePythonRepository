8298f1f4488c27ffd831a1ba0bca235b5de1fbec
Logan Loomis <27loganloomis@usd266.com>
2023-12-07 09:54:21 -0600

    did work

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -39,7 +39,96 @@ def count_target_letters(word,character):
 print(count_target_letters("apple", "a"))
 print(count_target_letters("banana", "a"))
 print(count_target_letters("", "a"))
+print("################")
+
+def in_alphabetical_order(letters):
+    previous = letters[0]
+    for letter in letters:
+        if letter < previous:
+            return False
+        previous = letter
+    return True
+
+print(in_alphabetical_order("abc"))
+print(in_alphabetical_order("cba"))
+print(in_alphabetical_order("defgh"))
+
+print("###############")
+def alternate_case(word):
+    
+    nextupper = True
+    result = ""
+    for letters in word:
+        if nextupper == True:
+            result = result + letters.upper()
+            nextupper = False
+        elif nextupper == False:
+            result = result + letters
+            nextupper = True
+    return result
 
 
-def in_alphabetical_order(word):
+
+print(alternate_case("python"))
+print(alternate_case("github"))
+print(alternate_case("copilot"))
+print("################")
+def remove_vowels(letters):
+    result = ""
+    for letter in letters:
+        if letter in "aeiou":
+            pass
+        else:
+            result = result + letter
+    return result
+
+print(remove_vowels("apple"))
+print(remove_vowels("banana"))
+print(remove_vowels("elephant"))
+
+print("#################")
+def to_camel_case(word):
+    result = ""
+    nextupper = False
+    for letter in word:
+        if nextupper == False:
+            result = result + letter.upper()
+            nextupper = False
+        elif nextupper == True:
+          result = result + letter  
+        nextupper = True
+    if result == " ":
+        result = result + letter.upper()
+        nextupper = False
+    return result
+
+print(to_camel_case("hello world"))
+print(to_camel_case("to camel case"))
+print(to_camel_case("another test case"))
+
+
+print("#################")
+def to_snake_case(word):
+    result = "_"
+    nextupper = True
+    for letter in word:
+        if nextupper == True:
+            result
+            nextupper = False
+        elif letter == " ":
+            pass
+            nextupper = True
+    return result
+
+print(to_snake_case("hello world"))
+print(to_snake_case("to camel case"))
+print(to_snake_case("another test case"))
+
+    
+
+
+
+
+
+
     
